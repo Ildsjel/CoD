7373dfb9d1af687a7ca8393aee3f69f0c68b63d1
Johannes Hoenscheid <jhoenscheid@trv2897.local>
2018-08-27 10:11:24 +0200

    Made all files available

---
+++ Desktop/Javascript_Learn/CoD_v1/deprecated/extJS.js
@@ -0,0 +1,29 @@
+/* To understand the value creation of the problem: Here are the output variables*/
+
+
+/*var redcos = document.getElementById("redcos");
+var avocos = document.getElementById("avocos");
+var increv = document.getElementById("increv");
+var prorev = document.getElementById("prorev");*/
+
+
+
+function valueType{
+
+					if (redcos.checked){
+						alert("costredution screen here");
+					}
+					else if (avocos.checked){
+						alert("avoidcost screen here");
+					}
+					else if (increv.checked){
+						alert("increv here");
+					}
+					else if (prorev.checked){
+						alert("prorev here");
+					}
+					else {
+						console.log("Error");
+					}
+}					
+
