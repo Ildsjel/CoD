1dca5d40106b1fecee8dfd905a28392866b5e457
Johannes Hoenscheid <jhoenscheid@trv2897.local>
2018-08-27 10:05:34 +0200

    Made all calcs available

--- Desktop/Javascript_Learn/CoD_v1/calcv3.js
+++ Desktop/Javascript_Learn/CoD_v1/calcv3.js
@@ -11,18 +11,43 @@ function valueType() {
 				document.write(redcostotal)
 				}
 		}
+
 	else if (avocos.checked){
 		var totalcostavoided = prompt("Please enter how much cost in total will be avoided", "10");
-		var totalcostavoided = prompt("Please enter when the cost will occur", "10.10.18");
+		var totalcosttime = prompt("Please enter when the cost will occur", "10.10.18");
+			
+			if (totalcostavoided != 0){
+				var avocostotal = totalcostavoided
+				document.write(avocostotal)
+				}
 		}
 	else if (increv.checked){
 		var totalrevenueincrease = prompt("Please enter how much revenue will increase per sales", "10");
 		var howmanysalesperweek = prompt("Please enter how much you can sell per week", "10");
+
+			if (totalrevenueincrease != 0){
+				var increvtotal = totalrevenueincrease*howmanysalesperweek
+				document.write(increvtotal)
+				}
 		}
+		
 	else if (prorev.checked){
 		var totalrevenueprotected = prompt("Please enter how much revenue will be protected per week", "10");
+
+			if (totalrevenueprotected != 0){
+				var prorevtotal = totalrevenueprotected
+				document.write(prorevtotal)
+				}
 		}
+
 	else {
 		console.log("Error");
 		}
+}
+
+function showValue() {
+	if (redcostotal!=0) {
+
+	}
+
 }
